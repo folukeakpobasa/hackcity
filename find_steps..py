fb0544e121d10412301f1502fdd49e7b56b3a54c
folukeakpobasa <folukeakpobasa@decagon.dev>
2022-10-06 12:53:31 +0100

    fix: number of step

--- find_steps..py
+++ find_steps..py
@@ -1,8 +1,18 @@
 
 def num_of_steps(num):
     steps = 0
-  
-    if num % 2 == 1:
-        floor_num = num // 2 
+    if num == 0 or num == 1:
+        return 0
+    while num > 0:
+        if num % 2 == 1:
+            floor_num = num // 2 
+            
+            num = floor_num - 1
+
+        else:
+            num /= 2
+            
+    
+   
         
             
