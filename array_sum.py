ad17c64c08290fdf5b7349e930b715eccf456ac7
folukeakpobasa <folukeakpobasa@decagon.dev>
2022-10-06 12:38:11 +0100

    find steps

--- array_sum.py
+++
@@ -1,7 +0,0 @@
-nums = [1, 2, 3, 4]
-sum_nums = []
-sum_nums.append(nums(0))
-for i in num:
-    if i > 0 :  
-        sum_nums.append(num(i)+)
-    
---
+++ find_steps..py
@@ -0,0 +1,8 @@
+
+def num_of_steps(num):
+    steps = 0
+  
+    if num % 2 == 1:
+        floor_num = num // 2 
+        
+            
