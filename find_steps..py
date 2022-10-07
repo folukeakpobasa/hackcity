707a23c81d0d0789b7528798c9d42c0e16d60c8b
folukeakpobasa <folukeakpobasa@decagon.dev>
2022-10-07 10:51:10 +0100

    feat: richest customer/Users/decagon/opt/anaconda3/bin/python /Users/decagon/Desktop/hackcity/richest_wealth.py

--- find_steps..py
+++ find_steps..py
@@ -1,20 +1,30 @@
 
 def num_of_steps(num):
     steps = 0
-    if num == 0 or num == 1:
+    if num == 0:
         return 0
     while num > 0:
-        if num % 2 == 1:
-            floor_num = num // 2 
-            
-            num = floor_num - 1
-            steps = steps + 1
-        else:
+        print (num)
+        # steps = steps + 1
+        if num % 2 == 0:
             num = num / 2
             steps = steps + 1
+            print(num)
+        else:
+            floor_num = num // 2
+            num = floor_num - 1
+            steps = steps + 1
+            print(num)
+
+        # if num % 2 == 1:
+        #     num = num // 2
+        #     steps = steps + 1
+
+        # else:
+        #     num = num / 2
+        #     steps = steps + 1
+
     return steps
 
+
 print(num_of_steps(14))
-   
-        
-            
---
+++ richest_wealth.py
@@ -0,0 +1,3 @@
+accounts = [[2, 8, 7], [7, 1, 3], [1, 9, 5]]
+sum_each_account = [sum(account) for account in accounts]
+print(max(sum_each_account))
