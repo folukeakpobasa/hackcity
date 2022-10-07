cbd458c1636e65b6bc77cd8708e3075cec92d146
folukeakpobasa <folukeakpobasa@decagon.dev>
2022-10-07 12:36:46 +0100

    fix: bug in find_steps.py

--- find_steps..py
+++ find_steps..py
@@ -4,27 +4,15 @@ def num_of_steps(num):
     if num == 0:
         return 0
     while num > 0:
-        print (num)
-        # steps = steps + 1
         if num % 2 == 0:
             num = num / 2
             steps = steps + 1
-            print(num)
+            
         else:
-            floor_num = num // 2
-            num = floor_num - 1
+            num = num - 1
             steps = steps + 1
-            print(num)
-
-        # if num % 2 == 1:
-        #     num = num // 2
-        #     steps = steps + 1
-
-        # else:
-        #     num = num / 2
-        #     steps = steps + 1
 
     return steps
 
 
-print(num_of_steps(14))
+print("Number of steps", num_of_steps(8))
